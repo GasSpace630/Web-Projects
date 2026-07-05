eb275e79a778673905848e14b0c519c4f94db76b
GasSpace630 <gasspace630@gmail.com>
2026-07-05 22:45:36 +0530

    styling, made the popup dialog cooler"

--- notes_app/script.js
+++ notes_app/script.js
@@ -1,4 +1,4 @@
-const saveDialog = document.getElementById("save-dialog");
+const promptDialog = document.getElementById("prompt-dialog");
 const noteNameInput = document.getElementById("note-name");
 const okBtn = document.getElementById("ok-btn");
 const cancelBtn = document.getElementById("cancel-btn")
@@ -11,8 +11,6 @@ document.querySelectorAll(".save-btn").forEach(button => {
 document.querySelectorAll(".open-btn").forEach(button => {
     button.addEventListener("click", open);
 });
-
-
 
 function getNoteName(callback) {
     noteNameInput.value = "";
@@ -33,27 +31,37 @@ function getNoteName(callback) {
 function save(event) {
     const noteEdit = event.target.closest(".note-edit");
     const textarea = noteEdit.querySelector(".editor");
+    const nameLbl = noteEdit.querySelector(".note-name-lbl");
+
+    promptDialog.hidden = false;
 
     getNoteName((key) => {
+        promptDialog.hidden = true;
         if (!key) return;
         localStorage.setItem(key, textarea.value);
-
+        nameLbl.textContent = key;
     });
 }
 
 function open(event) {
     const noteEdit = event.target.closest(".note-edit");
     const textedit = noteEdit.querySelector(".editor");
+    const nameLbl = noteEdit.querySelector(".note-name-lbl");
+    var text;
 
-    const key = prompt("Enter Note Name: ");
-    if (!key) return;
+    promptDialog.hidden = false;
 
-    const text = localStorage.getItem(key);
-    if (text == null) {
-        alert("Note not Found!!!");
-        return;
-    }
+    getNoteName((key) => {
+        promptDialog.hidden = true;
+        if (!key) return;
+        text = localStorage.getItem(key);
+        if (text == null) {
+            alert("Note not Found!!!");
+            return;
+        }
+        textedit.value = text;
+        nameLbl.textContent = key;
+    });
 
-    textarea.value = text;
 }
 
