72989ddb587c210c5eb527b71b4e7adcc729d350
GasSpace630 <gasspace630@gmail.com>
2026-07-14 23:12:04 +0530

    new notes in a new browser tab

--- notes_app/script.js
+++ notes_app/script.js
@@ -1,1 +1,30 @@
-const new_btn = document.getElementById("new-btn")
+const params = new URLSearchParams(window.location.search);
+
+const newBtn = document.getElementById("new-btn");
+const openBtn = document.getElementById("open-btn");
+const saveBtn = document.getElementById("save-btn");
+const saveAsBtn = document.getElementById("save-as-btn");
+const optionsBtn = document.getElementById("options-btn");
+const editor = document.getElementById("editor");
+
+let noteID = params.get("id");
+
+const savedText = localStorage.getItem(noteID);
+
+console.log(noteID);
+
+if (noteID == null) {
+    noteID = crypto.randomUUID();
+    history.replaceState({}, "", "?id=" + noteID);
+} else {
+    editor.value = savedText;
+}
+
+editor.addEventListener("input", function() {
+    localStorage.setItem(noteID, editor.value);
+});
+
+newBtn.addEventListener("click", function() {
+    const newID = crypto.randomUUID();
+    window.open("index.html?id" + newID, "_blank");
+});
