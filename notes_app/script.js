694ab809e94332d822013e91657c6d89ecd8e2ff
GasSpace630 <gasspace630@gmail.com>
2026-07-16 20:44:02 +0530

    back to simple local storage saving

--- notes_app/script.js
+++ notes_app/script.js
@@ -1,30 +1,77 @@
-const params = new URLSearchParams(window.location.search);
-
 const newBtn = document.getElementById("new-btn");
 const openBtn = document.getElementById("open-btn");
 const saveBtn = document.getElementById("save-btn");
 const saveAsBtn = document.getElementById("save-as-btn");
 const optionsBtn = document.getElementById("options-btn");
 const editor = document.getElementById("editor");
+const notesList = document.getElementById("notes-list");
 
-let noteID = params.get("id");
+const promptDialog = document.getElementById("prompt-dialog");
+const noteNameInput = document.getElementById("note-name");
+const okBtn = document.getElementById("ok-btn");
+const cancelBtn = document.getElementById("cancel-btn");
 
-const savedText = localStorage.getItem(noteID);
+// "save" , "open"
+let promptMode = "";
 
-console.log(noteID);
-
-if (noteID == null) {
-    noteID = crypto.randomUUID();
-    history.replaceState({}, "", "?id=" + noteID);
-} else {
-    editor.value = savedText;
+function saveNote(title, data) {
+    let notes = JSON.parse(localStorage.getItem("notes")) || [];
+    const index = notes.findIndex(note => note.title == title);
+    if (index != -1) {
+        notes[index].data = data;
+    } else {
+        notes.push({
+            title : title,
+            data : data,
+        });
+    }
+    localStorage.setItem("notes", JSON.stringify(notes));
 }
 
-editor.addEventListener("input", function() {
-    localStorage.setItem(noteID, editor.value);
+function loadNote(title) {
+    let notes = JSON.parse(localStorage.getItem("notes")) || [];
+    const note = notes.find(note => note.title == title);
+    return note || null;
+}
+
+function showPrompt(mode) {
+    promptMode = mode;
+    noteNameInput.value = "";
+    promptDialog.hidden = false;
+    noteNameInput.focus();
+}
+
+function hidePrompt() {
+    promptDialog.hidden = true;
+}
+
+okBtn.addEventListener("click", () => {
+    const title = noteNameInput.value.trim();
+    if (title == "") {
+        alert("Enter a note name!");
+    }
+    if (promptMode == "save") {
+        saveNote(title, editor.value);
+    }
+    if (promptMode == "open") {
+        const note = loadNote(title);
+        if (note) {
+            editor.value = note.data;
+        } else {
+            alert("Note not found!");
+        }
+    }
+    hidePrompt();
 });
 
-newBtn.addEventListener("click", function() {
-    const newID = crypto.randomUUID();
-    window.open("index.html?id" + newID, "_blank");
+cancelBtn.addEventListener("click", () => {
+    hidePrompt();
+});
+
+saveBtn.addEventListener("click", () => {
+    showPrompt("save");
+});
+
+openBtn.addEventListener("click", () => {
+    showPrompt("open")
 });
